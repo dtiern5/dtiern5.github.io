b8604cea14826bbf72c8101985e97a3d307ca619
dtiern5 <dannyjtierney@gmail.com>
2022-01-20 15:00:26 -0600

    added project videos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,40 +1,61 @@
-import React from 'react';
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
 import './styles/Projects.css';
 
 function Projects() {
+
     return (
         <section className="projects">
+
             <div>
                 <span className="section-span-projects"></span>
                 <h2 className="section-header-projects section-header-right">Projects</h2>
             </div>
             <div className="projects-container">
                 <div className="project-box">
-                    <h2>Butterfly Identifier</h2>
-                    <p>
-                        Identifies the species of a photographed butterfly.
-                        Written in Python using Tensorflow's DenseNet121 machine learning model.
-                        Try it out <a className="heroku-link" href="https://butterfly-recog.herokuapp.com/" target="_blank" rel="noopener noreferrer">here</a>!
-                    </p>
+                    <ReactPlayer controls="true" className="example-video" width="640px" url="https://youtu.be/7JfEymRDVqU" />
+                    <div className="project-text">
+                        <h2>Recipe Share</h2>
+                        <p>
+                            Users can add, edit, and delete recipes from their own recipe book,
+                            as well as share them with other users. Has light and dark mode functionality.
+                        </p>
+                        <p>
+                            Written with NodeJS, React, and connected to a Strapi backend.
+                        </p>
+                    </div>
+
                 </div>
                 <div className="project-box">
-                    <h2>JavaFX Appointment Scheduler</h2>
-                    <p>
-                        Allows CRUD interaction for users, appointments, and clients with a MySQL database.
-                        Automatic time-zone conversions and established EST business hours disallow scheduling conflicts.
-                    </p>
+
+                    <ReactPlayer controls="true" className="example-video" width="640px" url="https://youtu.be/8ogSebUvwMA" />
+                    <div className="project-text">
+                        <h2>Butterfly Identifier</h2>
+                        <p>
+                            Identifies the species in a photograph of a butterfly.
+                        </p>
+                        <p>
+                            Written in Python using Tensorflow's DenseNet121 machine learning model.
+                        </p>
+                    </div>
+
                 </div>
                 <div className="project-box">
-                    <h2>Recipe Share</h2>
-                    <p>
-                        Written with NodeJS, React, and connected to a Strapi backend.
-                        Users can add, edit, and delete recipes from their own recipe book,
-                        as well as share them with other users. Create your
-                        own <a className="heroku-link" href="/" target="_blank" rel="noopener noreferrer">here</a>!
-                    </p>
+                    <ReactPlayer controls="true" className="example-video" width="640px" url="https://youtu.be/tb4hLt7_SIo" />
+                    <div className="project-text">
+                        <h2>JavaFX Appointment Scheduler</h2>
+                        <p>
+                            Allows CRUD interaction for users, appointments, and clients with a MySQL database.
+                            Automatic time-zone conversions and established EST business hours disallow scheduling conflicts.
+                        </p>
+                        <p>
+                            MVC (Model - View - Controller) design pattern used, as well as comprehensive JavaDoc documentation.
+                        </p>
+                    </div>
                 </div>
+
             </div>
-            
+
         </section>
     )
 }
